e99f2f8ab437d374d6cbee089fd49781d62cd10c
dkuir <89020541+dkuir@users.noreply.github.com>
2021-08-21 05:55:48 +0530

    Crash

---
+++ plugins/alive.js
@@ -0,0 +1,11 @@
+const TREX = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Config = require('../config');
+let FM = Config.WORKTYPE == 'public' ? false : true
+
+TREX.addCMD({pattern: 'alive', fromMe: FM, deleteCommand: true,}, (async (message, match) => {
+
+var lasi = await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' })
+await message.client.sendMessage(message.jid, Buffer.from(lasi.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVETEXT+'\n         🔱🍁 ᴾᵒʷᵉʳᵈ ᴮʸ ᵀᴿᴱˣ 🍁🔱'})
+    }));
---
+++ plugins/antispam.js
@@ -0,0 +1,70 @@
+const Trex = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
+
+const code = ('A\nɴ\nᴛ\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n❰🍁🔱  T Rex BOT NEW  🔱🍁❱')
+const FINAL = "THIS IS AN ANTISAPM (anti lag),\nමෙම කේතය ඔබගේ දුරකථනය විකුර්ති පණිවිඩ නිසා සිරවිම වලකාලයි"
+const TMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම"
+const MUT = ".mute 1d"
+const SCXR = "Running script...."
+Trex.addCMD({pattern: 'antispam', fromMe: true, deleteCommand: false, desc: Ln,}, (async (message, match) => {
+
+  var msg = await message.reply('Preforming....');
+
+  await message.client.sendMessage(
+    message.jid,TMUTE, MessageType.text);
+
+    await message.client.sendMessage(
+      message.jid,MUT, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,SCXR, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,code, MessageType.text);
+
+    await message.client.sendMessage(
+        message.jid,code, MessageType.text);
+
+        await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                               await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                          await message.client.sendMessage(
+                             message.jid,FINAL, MessageType.text);
+
+  }));
